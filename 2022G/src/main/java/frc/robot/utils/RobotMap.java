5c2297a3a3b073be66963951521a4c7f222a039b
Aanya T <atolat-25@peddie.org>
2022-01-17 17:36:18 -0500

    Update RobotMap.java

--- 2022G/src/main/java/frc/robot/utils/RobotMap.java
+++ 2022G/src/main/java/frc/robot/utils/RobotMap.java
@@ -14,6 +14,8 @@ public class RobotMap {
   public static final int HOPPER_BELT = 10;
   public static final int TOWER_BELT_UPPER = 11;
   public static final int TOWER_BELT_LOWER = 12; 
+
+  
   public static final int SOLENOID_CLIMBER_LEFT = 1;
   public static final int SOLENOID_CLIMBER_RIGHT = 2;
 
