29136fe38d54b6822c7b3eedcee96d12a8903eec
VishnuGade <vishi.gade@gmail.com>
2022-02-12 12:55:15 -0500

    merged in dev and starting optimizing logging to gullinkambi

--- 2022G/src/main/java/frc/robot/Robot.java
+++ 2022G/src/main/java/frc/robot/Robot.java
@@ -114,9 +114,6 @@ public class Robot extends LoggedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
     lights.on();
   }
 
