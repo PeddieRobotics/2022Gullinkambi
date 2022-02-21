da78368b538488c76e3af6e50ed7c224d7193b1c
philiphan0109 <philiphan0109@hotmail.com>
2022-02-21 16:40:47 -0500

    Update Autonomous.java

--- 2022G/src/main/java/frc/robot/subsystems/Autonomous.java
+++ 2022G/src/main/java/frc/robot/subsystems/Autonomous.java
@@ -37,7 +37,7 @@ public class Autonomous extends SubsystemBase {
     private SendableChooser<Command> autoRoutineSelector;
     private Hashtable<String,Command> autoRoutines;
 
-    private Trajectory test, test2, test3, sPathTest;
+    private Trajectory test, sPathTest;
 
     public Autonomous() {
         autoRoutines = new Hashtable<String,Command>();
@@ -96,8 +96,6 @@ public class Autonomous extends SubsystemBase {
 
     public void setupAutoRoutines() {
         autoRoutines.put("Test Path", createCommandFromTrajectory(test));
-        autoRoutines.put("Test Path 2", createCommandFromTrajectory(test2));
-        autoRoutines.put("Test Path 3", createCommandFromTrajectory(test3));
         autoRoutines.put("Spath", createCommandFromTrajectory(sPathTest));
     }
 
@@ -106,46 +104,9 @@ public class Autonomous extends SubsystemBase {
     }
 
     private void defineAutoPaths(){
-        test = 
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d(Math.toRadians(0))),
-            List.of(new Translation2d(1, 0)),
-            new Pose2d(2, 0, new Rotation2d(Math.toRadians(0))),
-            configForward
-        );
+        test = getTransformedTrajectory(PathPlanner.loadPath("StraightPathTest", Constants.kMaxSpeedMetersPerSecond, Constants.kMaxAccelerationMetersPerSecondSquared));
         // test = getTransformedTrajectory(test);
-
-        test2 = 
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(
-                new Translation2d(1, -1),
-                new Translation2d(2, 1),
-                new Translation2d(3, 0)
-            ),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(4, 0, new Rotation2d(0)),
-            configForward
-        );
-
-                test3 = 
-        TrajectoryGenerator.generateTrajectory(
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(
-                new Translation2d(-1, 0.5),
-                new Translation2d(-1.5, 1),
-                new Translation2d(-2, 1.5),
-                new Translation2d(-3, 2)
-            ),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(-3,3, new Rotation2d(Math.toRadians(-90))),
-            configBackwards
-        );
-        // test = getTransformedTrajectory(test);
-
-        sPathTest = getTransformedTrajectory(PathPlanner.loadPath("SPathTest", Constants.kMaxSpeedMetersPerSecond, Constants.kMaxAccelerationMetersPerSecondSquared));
+        sPathTest = getTransformedTrajectory(PathPlanner.loadPath("SPathTest", Constants.kMaxSpeedMetersPerSecond, Constants.kMaxAccelerationMetersPerSecondSquared)); 
     }
  
     public SplitFFRamseteCommand createCommandFromTrajectory(Trajectory trajectory){
