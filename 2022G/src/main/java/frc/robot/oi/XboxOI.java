614bdd1ad08c58a42ce99157831646fe07a2d142
gwkoch <58572473+gwkoch@users.noreply.github.com>
2022-02-21 22:54:01 -0500

    further updates to xbox oi bindings

--- 2022G/src/main/java/frc/robot/oi/XboxOI.java
+++ 2022G/src/main/java/frc/robot/oi/XboxOI.java
@@ -2,6 +2,8 @@
 package frc.robot.oi;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.ClimbCommands.ExtendArm;
@@ -11,6 +13,8 @@ import frc.robot.commands.IntakeCommands.RunIntake;
 import frc.robot.commands.IntakeCommands.StopIntake;
 import frc.robot.commands.IntakeCommands.UnjamIntake;
 import frc.robot.commands.ShootCommands.ShootWithLL;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
 import frc.robot.commands.ShootCommands.ShootLayup;
 import frc.robot.commands.ShootCommands.ShootLow;
 import frc.robot.utils.Constants;
@@ -21,9 +25,14 @@ public class XboxOI {
 
     private static XboxOI oi;
     private Joystick xboxController;
+    private Drivetrain drivetrain;
+    private Intake intake;
 
     public XboxOI() {
         configureXboxControllers();
+
+        drivetrain = Drivetrain.getInstance();
+        intake = Intake.getInstance();
     }
 
     public static XboxOI getInstance() {
@@ -44,20 +53,17 @@ public class XboxOI {
 
         } else if (Constants.OI_CONFIG == OIConfig.XBOX_TEST) {
             xboxController = new Joystick(ControllerMap.XBOX_DRIVER_PORT);
-            // Driver xbox controller binds
-
+            // Driver xbox controller binds    
             new Button(() -> xboxController.getRawAxis(3) > 0.5).whenHeld(new ShootLayup()); // XBOX_RT
 
-            new Button(() -> xboxController.getRawAxis(2) > 0.5).whenPressed(new RunIntake()); // XBOX_LT
+            new Button(() -> xboxController.getRawAxis(2) > 0.5).toggleWhenPressed(new ConditionalCommand(new RunIntake(), new StopIntake(), intake::getIntakeSolenoid)); // XBOX_LT
 
-            new JoystickButton(xboxController, ControllerMap.XBOX_LB).whenPressed(new StopIntake()); // XBOX_LB
+            new JoystickButton(xboxController, ControllerMap.XBOX_LB).toggleWhenPressed(new ConditionalCommand(new InstantCommand(drivetrain::setToInverseMode, drivetrain), new InstantCommand(drivetrain::setToRegularMode, drivetrain), drivetrain::isInverseMode));
+            ; // XBOX_LB
 
             new JoystickButton(xboxController, ControllerMap.XBOX_A).whenHeld(new ShootWithLL()); // XBOX_A
 
-             new JoystickButton(xboxController, ControllerMap.XBOX_X).whenPressed(new ExtendArm()); // XBOX_X
-
-             new JoystickButton(xboxController, ControllerMap.XBOX_Y).whenPressed(new RetractArm()); // XBOX_Y
-
+             new JoystickButton(xboxController, ControllerMap.XBOX_X).whenHeld(new ExtendArm()).whenReleased(new RetractArm()); // XBOX_X
 
         }
     }
@@ -68,10 +74,6 @@ public class XboxOI {
 
     public double getTurn() {
         return xboxController.getRawAxis(ControllerMap.XBOX_RIGHT_STICK_X);
-    }
-
-    public boolean getInverseMode() {
-        return xboxController.getRawButton(ControllerMap.XBOX_RB);
     }
 
 }
