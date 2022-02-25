293460f30ffcd5af54b1fd1bc1f83b6e99505fb1
philiphan0109 <philiphan0109@hotmail.com>
2022-02-24 22:01:09 -0500

    Updated

--- 2022G/src/main/java/frc/robot/commands/AutoCommands/FourBallPathRight.java
+++ 2022G/src/main/java/frc/robot/commands/AutoCommands/FourBallPathRight.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.AutoCommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.SplitFFRamseteCommand;
 import frc.robot.commands.IntakeCommands.RunIntake;
 import frc.robot.commands.IntakeCommands.StopIntake;
@@ -12,10 +14,18 @@ import frc.robot.commands.ShootCommands.Target;
 public class FourBallPathRight extends  SequentialCommandGroup{
     public FourBallPathRight(SplitFFRamseteCommand part1, SplitFFRamseteCommand part2){
         addCommands(
-            part1,
-            new ShootForTimed(3),
-            part2,
-            new ShootWithLLForTime(3)
+            new ParallelCommandGroup(
+                new RunIntake(),
+                part1
+            ),
+            new StopIntake(),
+            new ShootForTimed(4),
+            new ParallelCommandGroup(
+                part2,
+                new RunIntake()
+            ),
+            new StopIntake(),
+            new ShootWithLLForTime(2)
         );
     }
 }
