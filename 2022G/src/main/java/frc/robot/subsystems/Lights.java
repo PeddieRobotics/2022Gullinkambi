619ee9051a664a511791bc61e09d70f21c6f9992
gwkoch <58572473+gwkoch@users.noreply.github.com>
2022-02-27 00:38:18 -0500

    updates to paths

--- 2022G/src/main/java/frc/robot/subsystems/Lights.java
+++ 2022G/src/main/java/frc/robot/subsystems/Lights.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.commands.LightCommands.FlashLights;
