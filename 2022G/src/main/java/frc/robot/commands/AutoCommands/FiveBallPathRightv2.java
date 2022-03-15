bbfd607f642d8fc8250609f879bddc931d0651aa
gwkoch <58572473+gwkoch@users.noreply.github.com>
2022-03-15 12:18:11 -0400

    5 ball auton edits

--- 2022G/src/main/java/frc/robot/commands/AutoCommands/FiveBallPathRightv2.java
+++ 2022G/src/main/java/frc/robot/commands/AutoCommands/FiveBallPathRightv2.java
@@ -13,15 +13,15 @@ public class FiveBallPathRightv2 extends SequentialCommandGroup{
     public FiveBallPathRightv2(Pose2d initialPose, SplitFFRamseteCommand part1, SplitFFRamseteCommand part2, SplitFFRamseteCommand part3, SplitFFRamseteCommand part4){
         addCommands(
             new ResetOdometry(initialPose),
-            new SetFlywheelRPM(2500),
+            new SetFlywheelRPM(2650),
             new ParallelCommandGroup(
                 new AutoIntakeWithHopper(0.7, 0.7),
                 part1
             ),
-            new ShootWithLLUntilEmpty(0.3),
+            new ShootWithLLUntilEmptyNoTarget(0.5),
             part2,
-            new AutoIntakeWithHopper(1.0, 1.0),
-            new ShootWithLLUntilEmpty(0.6),
+            new AutoIntakeWithHopper(0.7, 0.7),
+            new ShootWithLLUntilEmptyNoTarget(0.5),
             part3,
             new WaitCommand(1),
             part4,
---
+++ 2022G/src/main/java/frc/robot/commands/AutoCommands/ShootWithLLUntilEmptyNoTarget.java
@@ -0,0 +1,22 @@
+package frc.robot.commands.AutoCommands;
+
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.IntakeCommands.CheckIfHopperEmpty;
+import frc.robot.commands.ShootCommands.BlankCommand;
+import frc.robot.commands.ShootCommands.ShootWithLL;
+import frc.robot.commands.ShootCommands.Target;
+import frc.robot.subsystems.Drivetrain;
+
+public class ShootWithLLUntilEmptyNoTarget extends SequentialCommandGroup{
+
+    public ShootWithLLUntilEmptyNoTarget(double delay) {
+        addCommands(    
+            new ParallelRaceGroup(
+                new SequentialCommandGroup(new Target(), new ConditionalCommand(new ShootWithLL(true), new BlankCommand(), Drivetrain.getInstance()::isLockedOnTarget)),
+                new CheckIfHopperEmpty(delay))
+        );
+    }
+    
+}
