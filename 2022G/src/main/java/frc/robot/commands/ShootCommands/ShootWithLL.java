3d298086ca3809ca8d160521beb49adedefcec03
rohannunu <rnunugonda-22@peddie.org>
2022-04-07 11:22:28 -0400

    Made hopper speed changes (reverted to what it used to be)

--- 2022G/src/main/java/frc/robot/commands/ShootCommands/ShootWithLL.java
+++ 2022G/src/main/java/frc/robot/commands/ShootCommands/ShootWithLL.java
@@ -54,20 +54,7 @@ public class ShootWithLL extends CommandBase {
     // Check whether the speed of flywheel is good enough to shoot
     if (!flywheel.getShooterLock() && flywheel.isAtRPM(Constants.FLYWHEEL_THRESHOLD_SHOOTLL)) {
       flywheel.setShooterLock(true);
-      double dist = limelight.getDistance();
-      if(dist < 100){
-        hopper.setHopperVelocity(SmartDashboard.getNumber("Teleop: Hopper shoot LL speed", Constants.HOPPER_SHOOT_LL_SPEED));
-      }
-      else if(dist >= 100 && dist < 115){
-        hopper.setHopperVelocity(SmartDashboard.getNumber("Teleop: Hopper shoot LL speed", Constants.HOPPER_SHOOT_LL_SPEED) + 600);
-      }
-      else if(dist >= 115 & dist < 130){
-        hopper.setHopperVelocity(SmartDashboard.getNumber("Teleop: Hopper shoot LL speed", Constants.HOPPER_SHOOT_LL_SPEED) + 1200);
-      }
-      else if(limelight.getDistance() >= 130){
-        hopper.setHopperVelocity(SmartDashboard.getNumber("Teleop: Hopper shoot LL speed", Constants.HOPPER_SHOOT_LL_SPEED) + 1800);
-      }
-
+      hopper.setHopperVelocity(SmartDashboard.getNumber("Teleop: Hopper shoot LL speed", Constants.HOPPER_SHOOT_LL_SPEED));
     }
   }
 
