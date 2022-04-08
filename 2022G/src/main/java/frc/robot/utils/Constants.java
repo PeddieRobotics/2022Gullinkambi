e8bf35b43d1c088ea2736e7b01360fd0ceda5759
rohannunu <rnunugonda-22@peddie.org>
2022-04-08 17:20:13 -0400

    updated layup feed speed (hopper)

--- 2022G/src/main/java/frc/robot/utils/Constants.java
+++ 2022G/src/main/java/frc/robot/utils/Constants.java
@@ -36,7 +36,7 @@ public final class Constants {
 
   // Hopper constants
   public static final int HOPPER_MAX_CURRENT = 30; // amps
-  public static final double HOPPER_SHOOT_LAYUP_SPEED = -3000; // rpm
+  public static final double HOPPER_SHOOT_LAYUP_SPEED = -2200; // rpm
   public static final double HOPPER_SHOOT_LL_SPEED = -3800; // rpm
   public static final double HOPPER_SHOOT_POWER = 0.65;
   public static final double HOPPER_INDEX_POWER = 0.7;
